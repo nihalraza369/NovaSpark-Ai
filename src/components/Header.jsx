f563070784e9fd207fddfe5167b27405fa80cb36
nihalraza369 <nihalraza369@gmail.com>
2025-08-02 21:18:10 +0500

    Novaspark

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,7 +45,7 @@ import React, { useState, useEffect } from 'react';
         >
           <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-20">
-              <Link to="/" className="inline-block bg-white rounded-xl px-4 py-2 group">
+              <Link to="/" className="inline-block bg-white rounded-xl px-1 py-2 group">
               <div className="flex items-center text-2xl md:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-primary via-purple-900 to-pink-900 hover:opacity-80 transition-opacity">
                 <Zap size={32} className="mr-2 text-primary transform group-hover:scale-110 transition-transform duration-300" />
                 {siteName}
